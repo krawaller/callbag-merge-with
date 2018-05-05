8554d09dfb4c2ffd8d542653c296ac5e26746a58
David Waller <david@krawaller.se>
2018-05-05 17:05:26 +0200

    upgrade tests to mock v2

--- test.js
+++ test.js
@@ -3,13 +3,10 @@ const makeMockCallbag = require('callbag-mock');
 const mergeWith = require('./index');
 
 test('it merges the source with the other sources', t => {
-  let history = [];
-  const report = (name,dir,t,d) => t !== 0 && history.push([name,dir,t,d]);
-
-  const source = makeMockCallbag('source', true);
-  const newSource1 = makeMockCallbag('newSource1', true);
-  const newSource2 = makeMockCallbag('newSource2', true);
-  const sink = makeMockCallbag('sink', report);
+  const source = makeMockCallbag(true);
+  const newSource1 = makeMockCallbag(true);
+  const newSource2 = makeMockCallbag(true);
+  const sink = makeMockCallbag();
 
   mergeWith(newSource1, newSource2)(source)(0, sink);
 
@@ -17,57 +14,44 @@ test('it merges the source with the other sources', t => {
   newSource1.emit(1, 'bar');
   newSource2.emit(1, 'baz');
 
-  t.deepEqual(history, [
-    ['sink', 'body', 1, 'foo'],
-    ['sink', 'body', 1, 'bar'],
-    ['sink', 'body', 1, 'baz'],
-  ], 'sink gets data from all sources');
-
+  t.deepEqual(
+    sink.getReceivedData(),
+    ['foo','bar','baz'],
+    'sink gets data from all sources'
+  );
   t.end();
 });
 
 test('the merged source terminates once all contained sources terminate', t => {
-  let history = [];
-  const report = (name,dir,t,d) => t !== 0 && history.push([name,dir,t,d]);
-
-  const source = makeMockCallbag('source', true);
-  const newSource1 = makeMockCallbag('newSource1', true);
-  const newSource2 = makeMockCallbag('newSource2', true);
-  const sink = makeMockCallbag('sink', report);
+  const source = makeMockCallbag(true);
+  const newSource1 = makeMockCallbag(true);
+  const newSource2 = makeMockCallbag(true);
+  const sink = makeMockCallbag();
 
   mergeWith(newSource1, newSource2)(source)(0, sink);
 
-  source.emit(2, 'error');
+  source.emit(2);
+  t.ok(sink.checkConnection(), 'sink is still connected when 1/3 sources are terminated');
   newSource1.emit(2, 'moreError');
-  newSource2.emit(1, 'data');
+  t.ok(sink.checkConnection(), 'sink is still connected when 2/3 sources are terminated');
   newSource2.emit(2, 'finalError');
-
-  t.deepEqual(history, [
-    ['sink', 'body', 1, 'data'],
-    ['sink', 'body', 2, undefined],
-  ], 'sink is terminated after final source terminates');
-
+  t.ok(!sink.checkConnection(), 'sink is terminated when no sources remain');
   t.end();
 });
 
 test('a termination from sink ends all sources', t => {
-  let history = [];
-  const report = (name,dir,t,d) => t !== 0 && history.push([name,dir,t,d]);
-
-  const source = makeMockCallbag('source', report, true);
-  const newSource1 = makeMockCallbag('newSource1', report, true);
-  const newSource2 = makeMockCallbag('newSource2', report, true);
-  const sink = makeMockCallbag('sink');
+  const source = makeMockCallbag(true);
+  const newSource1 = makeMockCallbag(true);
+  const newSource2 = makeMockCallbag(true);
+  const sink = makeMockCallbag();
 
   mergeWith(newSource1, newSource2)(source)(0, sink);
 
   sink.emit(2);
 
-  t.deepEqual(history, [
-    ['source', 'talkback', 2, undefined],
-    ['newSource1', 'talkback', 2, undefined],
-    ['newSource2', 'talkback', 2, undefined],
-  ], 'sources all get requests from sink');
-
+  t.ok(
+    !source.checkConnection() && !newSource1.checkConnection() && !newSource2.checkConnection(),
+    'all sources are terminated by sink'
+  );
   t.end();
 });
